831fbbf5869ccd5382be72a5e396517b4c31dfc3
andypanagos <andypanagos2@gmail.com>
2022-07-31 17:28:25 -0400

    Updated With java

---
+++ js/Java.js
@@ -0,0 +1,15 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
+document.addEventListener("click", function(event){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false )
